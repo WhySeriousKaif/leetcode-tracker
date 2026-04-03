9c5ab66de1d43962c7a25da8da20f1278576c40f
MD Kaif Molla <139871820+WhySeriousKaif@users.noreply.github.com>
2026-04-03 13:12:02 +0530

    File updated at 03/04/2026, 13:12:01

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,30 +1,16 @@
-// Last updated: 03/04/2026, 13:10:24
+// Last updated: 03/04/2026, 13:12:01
 1class Solution {
-2    public int coinChange(int[] coins, int amount) {
+2    public int change(int amount, int[] coins) {
 3
-4        int n = coins.length;
-5        int[][] t = new int[n + 1][amount + 1];
+4        int[] dp = new int[amount + 1];
+5        dp[0] = 1; // base case
 6
-7        // Initialize
-8        for (int j = 1; j <= amount; j++) {
-9            t[0][j] = Integer.MAX_VALUE - 1;
-10        }
-11
-12        // Fill DP
-13        for (int i = 1; i <= n; i++) {
-14            for (int j = 1; j <= amount; j++) {
-15
-16                if (coins[i - 1] <= j) {
-17                    t[i][j] = Math.min(
-18                        1 + t[i][j - coins[i - 1]],  // take
-19                        t[i - 1][j]                  // not take
-20                    );
-21                } else {
-22                    t[i][j] = t[i - 1][j];
-23                }
-24            }
-25        }
-26
-27        return t[n][amount] >= Integer.MAX_VALUE - 1 ? -1 : t[n][amount];
-28    }
-29}
+7        for (int coin : coins) {
+8            for (int j = coin; j <= amount; j++) {
+9                dp[j] += dp[j - coin];
+10            }
+11        }
+12
+13        return dp[amount];
+14    }
+15}
